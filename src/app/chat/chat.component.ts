e8d04ee0becf071c486bf3ffd533f68674e0ca43
Pavel Sakoltsev <pashikus@gmail.com>
2023-05-20 13:02:38 +0300

    Add applyForms and submit function.

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Chatbotmessage } from '../chatbotmessage';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+            ReactiveFormsModule,        
+  ],
   templateUrl: 'chat.component.html' ,
   styleUrls: ['./chat.component.css']
 })
+
 export class ChatComponent {
+chatbotmessageList : Chatbotmessage[] = []  // List of messages
+applyForm = new FormGroup({
+  text: new FormControl('')
+});
+
+submitApplication() {
+    this.chatbotmessageList.push({
+                                    id: this.chatbotmessageList.length + 1,  
+                                    sender : 2,               // this place for user id
+                                    text : this.applyForm.value.text ?? ''
+                                  });
+    // 
+}
+
 
 }
